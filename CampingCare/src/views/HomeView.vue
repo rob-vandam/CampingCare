f55e53fd02ec247b2ee3530e7a59dad9c0af8531
rob <rob_vandam@gmx.com>
2024-12-18 17:20:57 +0100

    Initial setup

---
+++ CampingCare/src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+import { useRoute } from 'vue-router'
+import { onMounted } from 'vue'
+
+const route = useRoute()
+let testParam = null
+
+onMounted(() => {
+  testParam = route.query.test
+  console.log('Home1:', testParam)
+})
+
+// Gebruik productId om data op te halen of andere acties uit te voeren
+console.log('Home:', testParam)
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
